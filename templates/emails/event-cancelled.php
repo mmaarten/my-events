0c67cb8003d02253445285650245e6f8cef1f5ee
Maarten Menten <>
2021-07-02 16:39:53 +0200

    Cleanup.

--- templates/emails/event-cancelled.php
+++
@@ -1,6 +0,0 @@
-
-<p><?php esc_html_e('Following event is cancelled:', 'my-events'); ?></p>
-
-<p><a href="<?php echo esc_url(get_permalink($args['event']->ID)); ?>"><?php echo esc_html($args['event']->post_title); ?></a></p>
-
-<p><?php printf(esc_html__('Time: %s', 'my-events'), $args['event']->getTimeFromUntil()); ?></p>
