a2c457be604efa370d2750f177fb94d17196a217
Maarten Menten <>
2021-07-14 01:02:42 +0200

    Cleanup.

--- templates/emails/invitee-declined.php
+++ templates/emails/invitee-declined.php
@@ -1,5 +1,9 @@
+<?php
+
+namespace My\Events;
+
+?>
+
 <p><?php printf(esc_html__('%1$s declined your invitation for:', 'my-events'), $args['user']->display_name); ?></p>
 
-<p><a href="<?php echo esc_url(get_permalink($args['event']->ID)); ?>"><?php echo esc_html($args['event']->post_title); ?></a></p>
-
-<p><?php printf(esc_html__('Time: %s', 'my-events'), $args['event']->getTimeFromUntil()); ?></p>
+<?php Helpers::loadTemplate('emails/event-meta', $args); ?>
