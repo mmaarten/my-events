c469c287e273c498e8bf62ec429d55ca9dc7748e
Maarten Menten <>
2021-07-19 23:51:50 +0200

    Cleanup.

--- src/Calendar.php
+++ src/Calendar.php
@@ -8,6 +8,7 @@ use My\Events\Posts\Invitee;
 class Calendar
 {
     const SHORTCODE = 'calendar';
+    const TRANSIENT = 'my_events_calendar';
 
     /**
      * Init
@@ -16,8 +17,108 @@ class Calendar
     {
         add_action('wp_enqueue_scripts', [__CLASS__, 'registerAssets'], 0);
         add_action('wp_enqueue_scripts', [__CLASS__, 'autoEnqueueAssets'], 10);
+        add_action('wp_ajax_my_events_get_events', [__CLASS__, 'getEvents']);
+        add_action('wp_ajax_nopriv_my_events_get_events', [__CLASS__, 'getEvents']);
 
         add_shortcode(self::SHORTCODE, [__CLASS__, 'shortcode']);
+
+        add_action('acf/save_post', function ($post_id) {
+            if (get_post_type($post_id) == 'event') {
+                self::sanitizeTransient($post_id);
+            }
+        });
+
+        add_action('before_delete_post', function ($post_id) {
+            if (get_post_type($post_id) == 'event') {
+                self::sanitizeTransient($post_id);
+            }
+        });
+
+        add_action('transition_post_status', function ($new_status, $old_status, $post) {
+            if (get_post_type($object_id) == 'event') {
+                if (($old_status == 'publish' || $new_status == 'publish') && $old_status != $new_status) {
+                    self::sanitizeTransient($object_id);
+                }
+            }
+        }, 10, 3);
+
+        add_action('my_events/invitee_added', function ($invitee, $user_id, $event) {
+            self::sanitizeTransient($event);
+        }, 10, 3);
+
+        add_action('my_events/invitee_removed', function ($invitee, $user_id, $event) {
+            self::sanitizeTransient($event);
+        }, 10, 3);
+
+        add_action('added_post_meta', function ($object_id, $meta_key) {
+            if (get_post_type($object_id) == 'invitee') {
+                $invitee = new Invitee($object_id);
+                if (in_array($meta_key, ['status'])) {
+                    self::sanitizeTransient($invitee->getEvent());
+                }
+            }
+        }, 10, 2);
+
+        add_action('updated_post_meta', function ($meta_id, $object_id, $meta_key) {
+            if (get_post_type($object_id) == 'invitee') {
+                $invitee = new Invitee($object_id);
+                if (in_array($meta_key, ['status'])) {
+                    self::sanitizeTransient($invitee->getEvent());
+                }
+            }
+        }, 10, 3);
+    }
+
+    public static function sanitizeTransient($event_id)
+    {
+        if (! $event_id || get_post_type($event_id) != 'event') {
+            return;
+        }
+
+        error_log(__FUNCTION__);
+
+        $event = new Event($event_id);
+
+        $event_start = $event->getStartTime('U');
+        $event_end  = $event->getEndTime('U');
+
+        $transient = get_transient(self::TRANSIENT);
+
+        if (! is_array($transient)) {
+            $transient = [];
+        }
+
+        $sanitized = [];
+        foreach ($transient as $key => $events) {
+            list($user_id, $start, $end) = explode('|', $key);
+
+            $start = date('U', strtotime($start));
+            $end   = date('U', strtotime($end));
+
+            if (Helpers::doDatesOverlap($event_start, $event_end, $start, $end)) {
+                error_log("Removed from transient: $key");
+                continue;
+            }
+
+            $sanitized[$key] = $events;
+        }
+
+        set_transient(self::TRANSIENT, $sanitized);
+    }
+
+    /**
+     * Render
+     */
+    public static function render()
+    {
+        $options = apply_filters('my_events/calendar_options', [
+            'locale' => substr(get_locale(), 0, 2),
+        ]);
+
+        printf(
+            '<div id="calendar" data-options="%s"></div>',
+            esc_attr(json_encode($options))
+        );
     }
 
     /**
@@ -25,44 +126,49 @@ class Calendar
      */
     public static function getEvents()
     {
-        $now = new \DateTime('now', new \DateTimeZone(wp_timezone_string()));
-        $start = clone $now;
-        $end   = clone $now;
+        if (! wp_doing_ajax()) {
+            return;
+        }
 
-        $start->modify('-1 year');
-        $end->modify('+1 year');
+        $start = $_POST['start'];
+        $end   = $_POST['end'];
 
-        $start = $start->format('Y-m-d');
-        $end = $end->format('Y-m-d');
+        // Check cache.
+
+        $key = sprintf('%1$s|%2$s|%3$s', get_current_user_id(), $start, $end);
+
+        $transient = get_transient(self::TRANSIENT);
+
+        if (! is_array($transient)) {
+            $transient = [];
+        }
+
+        if (isset($transient[$key])) {
+            error_log("found in transient: $key");
+            wp_send_json(['events' => $transient[$key]]);
+        }
+
+        // Get posts.
 
         $posts = \My\Events\Model::getCalendarEvents($start, $end, get_current_user_id());
 
+        // Create events.
+
         $events = [];
+
         if (! is_wp_error($posts)) {
             foreach ($posts as $post) {
                 $events[] = self::createCalendarEvent($post);
             }
         }
 
-        return $events;
-    }
+        $transient[$key] = $events;
 
-    /**
-     * Render
-     *
-     * @link https://stackoverflow.com/a/1496165
-     */
-    public static function render()
-    {
-        $options = apply_filters('my_events/calendar_options', [
-            'locale' => substr(get_locale(), 0, 2),
-            'events' => self::getEvents(),
+        set_transient(self::TRANSIENT, $transient);
+
+        wp_send_json([
+            'events' => $events,
         ]);
-
-        printf(
-            '<div id="calendar" data-options="%s"></div>',
-            esc_attr(json_encode($options))
-        );
     }
 
     /**
@@ -108,6 +214,10 @@ class Calendar
             false,
             true
         );
+
+        wp_localize_script('my-events-calendar-script', 'MyEventsCalendarSettings', [
+            'ajaxurl' => admin_url('admin-ajax.php'),
+        ]);
 
         wp_register_style(
             'my-events-calendar-style',
