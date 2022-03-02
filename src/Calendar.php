8342d6433c108f510c2b4e12bc59ae0397c02364
Maarten Menten <>
2022-03-02 15:37:08 +0100

    Removed transient.

--- src/Calendar.php
+++ src/Calendar.php
@@ -8,7 +8,6 @@ use My\Events\Posts\Invitee;
 class Calendar
 {
     const SHORTCODE = 'calendar';
-    const TRANSIENT = 'my_events_calendar';
 
     /**
      * Init
@@ -21,113 +20,6 @@ class Calendar
         add_action('wp_ajax_nopriv_my_events_get_events', [__CLASS__, 'getEvents']);
 
         add_shortcode(self::SHORTCODE, [__CLASS__, 'shortcode']);
-
-        // add_action('acf/save_post', function ($post_id) {
-        //     if (get_post_type($post_id) == 'event') {
-        //         self::sanitizeTransient($post_id);
-        //     }
-        // });
-
-        add_action('before_delete_post', function ($post_id) {
-            if (get_post_type($post_id) == 'event') {
-                self::sanitizeTransient($post_id);
-            }
-        });
-
-        add_action('transition_post_status', function ($new_status, $old_status, $post) {
-            if (get_post_type($post) == 'event') {
-                if (($old_status == 'publish' || $new_status == 'publish') && $old_status != $new_status) {
-                    self::sanitizeTransient($post);
-                }
-            }
-        }, 10, 3);
-
-        add_action('post_updated', function ($post_id, $post_after, $post_before) {
-            switch (get_post_type($post_id)) {
-                case 'event':
-                    foreach (['post_title', 'post_name'] as $field) {
-                        if ($post_after->$field !== $post_before->$field) {
-                            self::sanitizeTransient($post_id);
-                            break;
-                        }
-                    }
-                    break;
-            }
-        }, 10, 3);
-
-        add_action('added_post_meta', function ($post_id, $meta_key) {
-            switch (get_post_type($post_id)) {
-                case 'event':
-                    if (in_array($meta_key, ['start', 'end', 'all_day', 'private'])) {
-                        self::sanitizeTransient($post_id);
-                    }
-                    break;
-                case 'invitee':
-                    $invitee = new Invitee($post_id);
-                    if (in_array($meta_key, ['status'])) {
-                        self::sanitizeTransient($invitee->getEvent());
-                    }
-                    break;
-            }
-        }, 10, 2);
-
-        add_action('updated_post_meta', function ($meta_id, $post_id, $meta_key) {
-            switch (get_post_type($post_id)) {
-                case 'event':
-                    if (in_array($meta_key, ['start', 'end', 'all_day', 'private'])) {
-                        self::sanitizeTransient($post_id);
-                    }
-                    break;
-                case 'invitee':
-                    $invitee = new Invitee($post_id);
-                    if (in_array($meta_key, ['status'])) {
-                        self::sanitizeTransient($invitee->getEvent());
-                    }
-                    break;
-            }
-        }, 10, 3);
-
-        add_action('my_events/invitee_added', function ($invitee, $user_id, $event) {
-            self::sanitizeTransient($event->ID);
-        }, 10, 3);
-
-        add_action('my_events/invitee_removed', function ($invitee, $user_id, $event) {
-            self::sanitizeTransient($event->ID);
-        }, 10, 3);
-    }
-
-    public static function sanitizeTransient($event_id)
-    {
-        if (! $event_id || get_post_type($event_id) != 'event') {
-            return;
-        }
-
-        $event = new Event($event_id);
-
-        $event_start = $event->getStartTime('U');
-        $event_end  = $event->getEndTime('U');
-
-        $transient = get_transient(self::TRANSIENT);
-
-        if (! is_array($transient)) {
-            $transient = [];
-        }
-
-        $sanitized = [];
-        foreach ($transient as $key => $events) {
-            list($user_id, $start, $end) = explode('|', $key);
-
-            $start = date('U', strtotime($start));
-            $end   = date('U', strtotime($end));
-
-            if (Helpers::doDatesOverlap($event_start, $event_end, $start, $end)) {
-                continue;
-            }
-
-            $sanitized[$key] = $events;
-        }
-
-        set_transient(self::TRANSIENT, $sanitized);
     }
 
     /**
@@ -157,20 +49,6 @@ class Calendar
         $start = $_POST['start'];
         $end   = $_POST['end'];
 
-        // Check cache.
-
-        $key = sprintf('%1$s|%2$s|%3$s', get_current_user_id(), $start, $end);
-
-        $transient = get_transient(self::TRANSIENT);
-
-        if (! is_array($transient)) {
-            $transient = [];
-        }
-
-        if (isset($transient[$key])) {
-            wp_send_json(['events' => $transient[$key]]);
-        }
-
         // Get posts.
 
         $posts = \My\Events\Model::getCalendarEvents($start, $end, get_current_user_id());
@@ -184,10 +62,6 @@ class Calendar
                 $events[] = self::createCalendarEvent($post);
             }
         }
-
-        $transient[$key] = $events;
-
-        set_transient(self::TRANSIENT, $transient);
 
         wp_send_json([
             'events' => $events,
